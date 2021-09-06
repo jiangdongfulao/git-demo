5e8c5fe48f4abe0324bd66bebfb489ba83f7d264
52Hertz <52Hertz@163.com>
2021-09-06 12:33:36 +0800

    first commit

---
+++ src/main/java/xiaojiang/li/App.java
@@ -0,0 +1,13 @@
+package xiaojiang.li;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
